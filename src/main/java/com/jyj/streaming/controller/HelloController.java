d368df8b46f66eac74e6a93ab26b63afaa21e440
Yeong Jun Joo <juyj7282@gmail.com>
2020-09-28 13:54:10 +0900

    setting finished

---
+++ src/main/java/com/jyj/streaming/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.jyj.streaming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello.do")
+	public String hello(Model model) {
+		model.addAttribute("greeting", "안녕하세요");
+		return "hello";
+	}
+}
