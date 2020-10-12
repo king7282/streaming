f5f9ab3a01fff4a9719499b562e70bc7e523343a
Yeong Jun Joo <juyj7282@gmail.com>
2020-10-12 15:47:47 +0900

    add MainController, BroadcastController for testing 3 view

---
+++ src/main/java/com/jyj/streaming/controller/BroadcastController.java
@@ -0,0 +1,22 @@
+package com.jyj.streaming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/Broadcast")
+public class BroadcastController {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String broadcast(Model model) {
+		return "broadcast";
+	}
+	
+	@RequestMapping(value="/{ID}", method=RequestMethod.GET)
+	public String watchBroadcast(Model model, String ID) {
+		model.addAttribute("ID", ID);
+		return "watch_broadcast";
+	}
+}
